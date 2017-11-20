f018f8689840013113de0e83c3ba994b042f09bf
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-11-20 10:54:10 +0100

    Tilføjet DB connector

---
+++ src/DB_Connector.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_Connector {
+    //  Declare a connection
+    private static Connection con = null;
+    //  JDBC driver
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    //  The url = jdbc:dbms//host name:port#/db name
+    private static String url = "jdbc:mysql://10.200.128.46:3306/";
+    //  User name
+    private static String user = "root";
+    // Password
+    private static String password = "12345678";
+
+    public static Connection connect() {
+        System.out.println("\n--Connecting to MySQL JDBC--");
+        //  Locate MySQL JDBC Driver
+        try {
+            Class.forName(DRIVER);
+        }
+        //  Catch exceptions if JDBC is not found
+        catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("\n--JDBC driver is missing--");
+
+        }
+        System.out.println("\n--MySQL JDBC driver registered--");
+
+        try {
+            //  Connect to MySQL DB = URL, userName, password
+            con = DriverManager.getConnection(url, user, password);
+        }
+        // Catch exceptions on connection error
+        catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("\n--Did not connect, try again--");
+        }
+        //   if connecting successful
+        if (con != null) {
+            System.out.println("\n--Connection successful--");
+        } else {
+            System.out.println("\n--Failed to connect--");
+        }
+        return con;
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Team One!");
+        Connection con = DB_Connector.connect();
     }
 }
