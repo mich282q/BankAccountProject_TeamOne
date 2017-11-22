de30e3403e8e79b1eaf12cd75054aacff5d0f1dc
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-11-22 13:18:43 +0100

    Tilføjet alt muligt  lækkert

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,39 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
 
 public class Controller {
 
     @FXML
-    private Button btnGennemførOverførsel;
+    private Button btnGennemfoerOverfoersel;
+    @FXML
+    private TextField txfBeloeb;
+    @FXML
+    private MenuItem miAction1; //Overfør fra
+    @FXML
+    private MenuItem miAction2; //Overfør fra
+    @FXML
+    private MenuItem miAction3; //Overfør til
+    @FXML
+    private MenuItem miAction4; //Overfør til
+    @FXML
+    private TableView tableView;
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+        int num1;
+
+        if(e.getSource()== btnGennemfoerOverfoersel) {
+            System.out.println("Knappen virker :-)");
+        }
+        num1=Integer.parseInt(txfBeloeb.getText());
+    }
+
 
 
 
