980f144c533941c7be85ef86a9936d63a8f7917f
mich282q <31951247+mich282q@users.noreply.github.com>
2017-11-27 11:39:30 +0100

    ændret users

--- src/Bruger.java
+++ src/Bruger.java
@@ -8,12 +8,13 @@ public class Bruger {
     static Statement stmt = null;
 
     // Laver en bruger med navn, adresse og id
-    public static void lavBruger(String fnavn,String lnavn, String adresse) throws SQLException {
+    public static void lavBruger(int Person_id, String fnavn,String lnavn, String adresse) throws SQLException {
 
         System.out.println("Creating statement...");
         stmt = con.createStatement();
         String sql;
-        sql = "INSERT INTO bruger (fnavn,lnavn, adresse) VALUES ('" + fnavn + "', '" + lnavn + "', '" + adresse + "');";
+        //AI (fnavn, lnavn, adresse)
+        sql = "INSERT INTO bruger  VALUES ("+Person_id +",'" + fnavn + "', '" + lnavn + "', '" + adresse + "');";
         System.out.println(sql);
         stmt.execute(sql);
         System.out.println("Successful!");
@@ -23,11 +24,11 @@ public class Bruger {
 
     public static void insertBrugerData() throws SQLException {
 
-        lavBruger( "Erik", "Ruhmanis", "Troensevej");
-        lavBruger("Thomas", "Christensen", "Parkvej");
-        lavBruger( "Michael", "Trans", "Danavej");
-        lavBruger( "Daniel", "Nørd", "Femøvej");
-        lavBruger( "Burhan", "Öztürk", "Maglemølle");
+        lavBruger( 1,"Erik", "Ruhmanis", "Troensevej");
+        lavBruger(2,"Thomas", "Christensen", "Parkvej");
+        lavBruger( 3,"Michael", "Trans", "Danavej");
+        lavBruger( 4,"Daniel", "Nørd", "Femøvej");
+        lavBruger( 5,"Burhan", "Öztürk", "Maglemølle");
 
     }
 }
