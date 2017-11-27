92fd93aa08ee132a47c87095891eb063af6c6477
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-11-27 09:54:51 +0100

    Tilføjet alt muligt  lækkert

--- src/Konto.java
+++ src/Konto.java
@@ -1,2 +1,24 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Konto {
+
+    static Connection con = DB_Connector.connect();
+    static Statement stmt = null;
+
+    // Laver en bruger med navn, adresse og id
+    public static void lavKonto(String type, int reg_nr, int konto_nr, int rentesats, int saldo, int overtraeksgebyr, int overtraek, int id) throws SQLException {
+
+        System.out.println("Creating statement...");
+        stmt = con.createStatement();
+        String sql;
+        sql = "INSERT INTO bruger VALUES ('" + type + "', " + reg_nr + ", " + konto_nr + ", " + rentesats
+                + ", " + saldo + ", " + overtraeksgebyr + ", "+ overtraek + ", "+ id +" );";
+        System.out.println(sql);
+        stmt.execute(sql);
+        System.out.println("Successful!");
+
+    }
+
 }
