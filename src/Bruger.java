5090184774e03c79fcd02afd2a157ee5395b0e2e
mich282q <31951247+mich282q@users.noreply.github.com>
2017-11-27 08:43:52 +0100

    ændret i dokumenter og lavet ændring i database

--- src/Bruger.java
+++ src/Bruger.java
@@ -8,12 +8,12 @@ public class Bruger {
     static Statement stmt = null;
 
     // Laver en bruger med navn, adresse og id
-    public static void lavBruger(int id, String navn, String adresse) throws SQLException {
+    public static void lavBruger(int Person_id, String navn, String adresse) throws SQLException {
 
         System.out.println("Creating statement...");
         stmt = con.createStatement();
         String sql;
-        sql = "INSERT INTO bruger VALUES (" + id + ", '" + navn + "', '" + adresse + "');";
+        sql = "INSERT INTO bruger VALUES (" + Person_id + ", '" + navn + "', '" + adresse + "');";
         System.out.println(sql);
         stmt.execute(sql);
         System.out.println("Successful!");
